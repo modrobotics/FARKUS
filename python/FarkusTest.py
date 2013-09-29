1e15cfff3f1574ef893cc0a5ddb8a2da91293553
Jonathan Moyes <jmoyes@modrobotics.com>
2013-09-29 14:30:58 -0600

    FarkusTest and FarkusTestResult skeletons

---
+++ python/FarkusTest.py
@@ -0,0 +1,6 @@
+class FarkusTest():
+    "Class to define the tests that can be performed by FarkusModules on FarkusParts on a FarkusTable fark. Farking FarkFarker.  Fark us."
+    # Eventually this class should be expanded and generalized to extend a "FarkusActions" class.  But that, my friends, is for another day.
+    def __init__(self, partType):
+        self.id = 1  #TODO: Generate UUID
+        
